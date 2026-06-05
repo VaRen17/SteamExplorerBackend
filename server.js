5ae2e86ea25c2b03e076c5477a65b35fb03631cd
V. Fery <ferenczvargaakos@gmail.com>
2026-06-05 14:09:18 +0300

    Updated backend

--- server.js
+++ server.js
@@ -18,8 +18,33 @@ app.get('/api/mostplayed', async (req, res) => {
   try {
     const response = await fetch(MOST_PLAYED_URL, { headers });
     const data = await response.json();
-    res.json(data);
+
+    const ranks = data.response.ranks;
+
+    const finalGames = [];
+
+    for (const game of ranks) {
+      const appid = game.appid;
+
+      const detailsRes = await fetch(DETAILS_URL + appid, { headers });
+      const detailsJson = await detailsRes.json();
+
+      const details = detailsJson[appid]?.data;
+
+      if (!details) continue;
+
+      finalGames.push({
+        appid: appid,
+        name: details.name,
+        peak: game.peak_in_game,
+        image: details.capsule_imagev5 || details.capsule_image || details.header_image
+      });
+    }
+
+    res.json(finalGames);
+
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: "Steam API error" });
   }
 });
